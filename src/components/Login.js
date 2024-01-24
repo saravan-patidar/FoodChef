5e43e9edda7a1a15202d2cb5cc289c32d248e18d
saravan-patidar <shravanpatidar.48@gmail.com>
2024-01-24 19:35:27 +0530

    new

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,65 +24,64 @@ const Login = () => {
       resetForm.resetForm();
     },
   });
+  const { handleChange, handleBlur, handleSubmit, touched, errors } = formik;
   return (
-    ({ handleChange, handleBlur, handleSubmit, touched, errors } = formik),
-    (
-      // console.log(formik.values),
-      <div className="h-[80vh] flex justify-center items-center ">
-        <div className="mt-24 my-5  w-2/6 bg-orange-300  shadow-xl shadow-slate-200 m-auto border rounded-xl">
-          <h2 className="font-bold text-center text-xl p-3 bg-slate-700 text-white  rounded-t-xl ">
-            LOGIN
-          </h2>
-          <form className="p-7 text-center" onSubmit={handleSubmit}>
-            <div className="my-2 flex flex-col w-4/6 m-auto">
-              <label htmlFor="email" className="text-xl font-bold text-left">
-                Email
-              </label>
-              <input
-                className="p-2 my-2 outline-none rounded-lg font-semibold"
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="off"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={formik.values.email}
-              />
-              {errors.email && touched.email ? (
-                <div className="text-red-500 font-bold text-center">
-                  {errors.email}
-                </div>
-              ) : null}
-            </div>
-            <div className="my-2 flex flex-col w-4/6 m-auto">
-              <label htmlFor="password" className="text-xl font-bold text-left">
-                Password
-              </label>
-              <input
-                className="p-2 my-2 outline-none rounded-lg"
-                type="text"
-                name="password"
-                id="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={formik.values.password}
-              />
-              {errors.password && touched.password ? (
-                <div className="text-red-500 font-bold text-center ">
-                  {errors.password}
-                </div>
-              ) : null}
-            </div>
-            <button
-              type="submit"
-              className=" p-2 m-auto w-3/6 font-bold tetx-xl text-center bg-blue-400 rounded-full hover:bg-blue-700"
-            >
-              Login
-            </button>
-          </form>
-        </div>
+    // ({ handleChange, handleBlur, handleSubmit, touched, errors } = formik),
+    // console.log(formik.values),
+    <div className="h-[80vh] flex justify-center items-center ">
+      <div className="mt-24 my-5  w-2/6 bg-orange-300  shadow-xl shadow-slate-200 m-auto border rounded-xl">
+        <h2 className="font-bold text-center text-xl p-3 bg-slate-700 text-white  rounded-t-xl ">
+          LOGIN
+        </h2>
+        <form className="p-7 text-center" onSubmit={handleSubmit}>
+          <div className="my-2 flex flex-col w-4/6 m-auto">
+            <label htmlFor="email" className="text-xl font-bold text-left">
+              Email
+            </label>
+            <input
+              className="p-2 my-2 outline-none rounded-lg font-semibold"
+              type="email"
+              name="email"
+              id="email"
+              autoComplete="off"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={formik.values.email}
+            />
+            {errors.email && touched.email ? (
+              <div className="text-red-500 font-bold text-center">
+                {errors.email}
+              </div>
+            ) : null}
+          </div>
+          <div className="my-2 flex flex-col w-4/6 m-auto">
+            <label htmlFor="password" className="text-xl font-bold text-left">
+              Password
+            </label>
+            <input
+              className="p-2 my-2 outline-none rounded-lg"
+              type="text"
+              name="password"
+              id="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={formik.values.password}
+            />
+            {errors.password && touched.password ? (
+              <div className="text-red-500 font-bold text-center ">
+                {errors.password}
+              </div>
+            ) : null}
+          </div>
+          <button
+            type="submit"
+            className=" p-2 m-auto w-3/6 font-bold tetx-xl text-center bg-blue-400 rounded-full hover:bg-blue-700"
+          >
+            Login
+          </button>
+        </form>
       </div>
-    )
+    </div>
   );
 };
 export default Login;
